1fdad60e20fd60ba63597b1b0940dd1bcc74fae6
Megan Keesee <hello@megankeesee.com>
2019-05-02 17:54:12 -0700

    update proxy for AWS

--- server.js
+++ server.js
@@ -2,16 +2,19 @@ require('newrelic');
 const express = require('express');
 const path = require('path');
 const axios = require('axios');
-const redis = require('redis');
+// const redis = require('redis');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
+const cors = require('cors');
+const morgan = require('morgan');
+// const client = redis.createClient();
 
-const client = redis.createClient();
+// client.on('error', (err) => {
+//   console.log("Error " + err);
+// });
 
-client.on('error', (err) => {
-  console.log("Error " + err);
-});
-
+app.use(cors());
+app.use(morgan())
 app.use('/', express.static(path.join(__dirname, 'public')));
 app.use('/stocks/:ticker', express.static(path.join(__dirname, 'public')));
 
@@ -19,27 +22,34 @@ const axios3001 = axios.create({
   baseURL: 'http://54.213.221.102:3001',
 });
 
-app.get('/api/:ticker', (req, res) => {
-  return client.get(`/api/${req.params.ticker}`, (err, result) => {
-    // If that key exist in Redis store
-    if (result) {
-      const resultJSON = JSON.parse(result);
-      return res.status(200).json(resultJSON);
-    } else {
-      return axios3001.get(`/api/${req.params.ticker}`)
-      .then(response => {
-        const responseJSON = response.data;
-        // Save the API response in Redis store
-        client.setex(`/api/${req.params.ticker}`, 3600, JSON.stringify({ source: 'Redis Cache', ...responseJSON, }));
-        // Send JSON response to client
-        return res.status(200).json();
-      })
-      .catch(err => {
-        return res.json(err);
-      })
-    }
-  })
-});     
+app.use('/api/:ticker', (req, res) => {
+  axios3001.get(`/api/${req.params.ticker}`)
+    .then(response => res.send(response.data))
+    .catch(err => res.send(err));
+})
+
+// app.get('/api/:ticker', (req, res) => {
+//   console.log('wtf');
+//   return client.get(`/api/${req.params.ticker}`, (err, result) => {
+//     // If that key exist in Redis store
+//     if (result) {
+//       const resultJSON = JSON.parse(result);
+//       return res.status(200).json(resultJSON);
+//     } else {
+//       return axios3001.get(`/api/${req.params.ticker}`)
+//       .then(response => {
+//         const responseJSON = response.data;
+//         // Save the API response in Redis store
+//         client.setex(`/api/${req.params.ticker}`, 3600, JSON.stringify({ source: 'Redis Cache', ...responseJSON, }));
+//         // Send JSON response to client
+//         return res.status(200).json();
+//       })
+//       .catch(err => {
+//         return res.json(err);
+//       })
+//     }
+//   })
+// });     
 
 
 app.get('/api/history/:ticker', (req, res) => {
